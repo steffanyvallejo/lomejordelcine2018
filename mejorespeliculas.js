c4f0e5a112699be520b1fc24a9bf39603737749d
Steffany Vallejo <stefyvallejo.vallejo@gmail.com>
2019-01-21 21:46:04 -0500

    Base

---
+++ mejorespeliculas.js
@@ -0,0 +1,123 @@
+/* Cargar las noticias de noticias.json y noticias.xml */
+var div;
+function addNew(titulo, url, genero, descripcion) {
+  
+  /*autor*/
+  var title = $("<p/>", {
+    "class": "card-title col-md-12 col-sm-12 col-12 ",
+    html: titulo
+  })
+
+  title.addClass("titulo");
+
+  /*contenido*/
+  var p = $("<p />", {
+   
+    "class": "contenido card-title col-md-12 col-12  col-sm-12 py-0 py+0 mx+0 my ",
+    
+    html: genero
+  })
+
+   p.addClass("contenido");
+
+  var duracion = $("<p />", {
+   
+    "class": "contenido card-title col-md-12 col-12  col-sm-12 py-0 py+0 mx+0 my ",
+    
+    html: url +" minutos"
+  })
+
+
+  /*logo*/
+   var imagen = $("<img/>", {
+    "class": "card-text col-sm-12 col-md-12 col-12 img-fluid",
+    "src": String(descripcion),
+    "style":"max-height:200px; max-width:150px; min-height:40px; min-width:50px",
+    html: imagen
+
+  })
+
+  /*div para la imagen*/
+  var i = $("<div/>", {
+    "class": "card-text col-md-2 col-sm-3 col-2"
+  });
+
+/*contenedor de todos los tweets*/
+  div = $("<div/>", {
+    "class": "card-body  col-md-12 col-sm-12 col-12 row border-top px-2 pt-2 mx-0"
+
+  });
+  
+/*contenedor de un tweet*/
+  var formato = $("<div/>", {
+    "class": "card-text col-md-12 col-12 col-sm-12 row my-0"
+  });
+
+  /*contenedor de la segunda columna del tweet*/
+  var columnas = $("<div/>", {
+    "class": "card-text col-md-10 col-sm-9 col-10 px-0 px+0"
+  });
+
+  imagen.appendTo(i);
+  title.appendTo(columnas);
+  p.appendTo(columnas);
+  duracion.appendTo(columnas);
+  i.appendTo(formato);
+  columnas.appendTo(formato);
+  formato.appendTo(div);
+ 
+  
+  div.appendTo("#peliculas");
+}
+
+
+
+
+function loadPeliculasJson(){
+  $.getJSON("csvjson.json", function (data) {
+    for (var i = 0; i < data.length; i++) {
+      addNew(data[i]['TITULO'], data[i]['DURACION'], data[i]['GENERO'],  data[i]['IMAGEN'])
+    }
+  });
+}
+
+
+$(document).ready(function () {
+
+loadPeliculasJson();
+  $("button").click(function (e) {
+     
+    var texto = $('input#buscador').val();
+
+
+    if (texto.length != 0) {
+     
+      document.getElementById('texto').innerHTML = texto;
+      
+      $('#peliculas .card-body').filter(function (index) {
+       
+        $(this).show();
+
+        var t = $(this).text()
+        if (t.indexOf(texto) == -1) {
+          $(this).hide()
+
+        }
+
+      });
+
+    } else {
+      
+      $('#peliculas .card-body').each(function () {
+        $(this).hide();
+        document.getElementById('texto').innerHTML = "";
+        
+        
+      });
+    } return false;
+
+
+
+  })
+});
+
